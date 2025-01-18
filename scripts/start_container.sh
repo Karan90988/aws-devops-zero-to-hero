4dcb69a6bed148abbeb2e7288a12dae2f513be36
Karan90988 <110415169+Karan90988@users.noreply.github.com>
2025-01-19 01:08:34 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull karan90988/simple-python-flask-build
-
+#hwds
 # Run the Docker image as a container
 docker run -d -p 5000:5000 karan90988/simple-python-flask-build  
